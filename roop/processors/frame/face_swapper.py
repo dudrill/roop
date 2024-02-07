954f5a3c6ead9ea464b5c123394360d08d30c524
dudrill <82663793+dudrill@users.noreply.github.com>
2024-02-07 20:12:40 +0300

    Update face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -16,8 +16,8 @@ NAME = 'ROOP.FACE-SWAPPER'
 
 
 def pre_check() -> bool:
-    download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/lkw99/roop_model/resolve/main/inswapper_128.onnx'])
+    # download_directory_path = resolve_relative_path('../models')
+    # conditional_download(download_directory_path, ['https://huggingface.co/lkw99/roop_model/resolve/main/inswapper_128.onnx'])
     return True
 
 
